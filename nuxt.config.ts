8565186fd9b8fc910a74f1cf6bb5f0da568f3d3d
frehsegustavo <frehsegustavo@gmail.com>
2023-12-26 17:10:14 -0300

    [add] Configs do Vuetify3 e favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,41 @@
+// Imports from Nuxt
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// Exports from Nuxt
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+	// Habilita o modo de DEV TOOLS no projeto - depuracao e etc
+	devtools: { enabled: true },
+
+	// Define o cabeçalho do Nuxt
+	app: {
+		head: {
+			link: [
+				{
+					rel: "icon",
+					type: "image/ico",
+					href: "favicon.ico",
+				},
+			],
+		},
+	},
+
+	build: {
+		transpile: ["vuetify"],
+	},
+	modules: [
+		(_options, nuxt) => {
+			nuxt.hooks.hook("vite:extendConfig", (config) => {
+				// @ts-expect-error
+				config.plugins.push(vuetify({ autoImport: true }));
+			});
+		},
+	],
+	vite: {
+		vue: {
+			template: {
+				transformAssetUrls,
+			},
+		},
+	},
+});
