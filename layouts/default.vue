0c1deeb9c503f947e33ce67ec80d74ad459e4d09
frehsegustavo <frehsegustavo@gmail.com>
2023-12-26 23:14:27 -0300

    [add] Subscribe e footer no layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
-	<v-app>
+	<v-app full-height>
 		<NuxtPage></NuxtPage>
+		<Subscribe />
+		<Footer />
 	</v-app>
 </template>
 
-<script></script>
+<script setup>
+import Footer from "~/components/sections/layout/Footer.vue";
+import Subscribe from "~/components/sections/layout/Subscribe.vue";
+</script>
