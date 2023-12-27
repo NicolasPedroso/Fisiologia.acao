21d1f8b195a19bee196aef7620bc756a0c791526
frehsegustavo <frehsegustavo@gmail.com>
2023-12-26 23:14:47 -0300

    [add] Headers e formatação básica

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-	<div>{{ objetoAleatorio }}</div>
-	<sectionsBanner />
+	<Head>
+		<Title>Página inicial</Title>
+		<meta
+			name="description"
+			content="Página inicial do projeto base 2024 da Ecomp."
+		/>
+	</Head>
+	<v-container fluid class="ma-0 pa-0 text-center">
+		Bem vindo ao projeto base de 2024 :)
+	</v-container>
 </template>
-<script>
-export default {
-	data: () => ({
-		objetoAleatorio: "teste",
-	}),
-};
-</script>
+<script setup></script>
