ac0946c380a5b82099be9440677090fffd7bab23
frehsegustavo <frehsegustavo@gmail.com>
2023-12-28 18:31:05 -0300

    [add] Componente de título para página de autenticação

---
+++ components/sections/auth/TitleAuth.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="d-flex flex-column align-center">
+		<v-img
+			src="/imagens/logo.png"
+			alt="Logo da Ecomp"
+			contain
+			class="auth-logo mb-3"
+		/>
+		<span class="auth-title"> {{ title }} </span>
+	</div>
+</template>
+<script setup>
+const props = defineProps({
+	title: {
+		type: String,
+		required: true,
+	},
+});
+</script>
+<style scoped>
+.auth-logo {
+	width: 75px;
+	height: 112px;
+	filter: grayscale(100%);
+}
+
+.auth-title {
+	font-family: Poppins !important;
+	font-weight: bold;
+	font-size: 1.5rem;
+	color: rgb(113, 113, 113);
+	text-transform: capitalize;
+}
+</style>
