4d88fd72b3b434e173fb7b84d766bdfb42a86ae7
frehsegustavo <frehsegustavo@gmail.com>
2023-12-30 15:17:35 -0300

    [add] Botão que retorna para o topo terminado

---
+++ components/sections/layout/ButtonTop.vue
@@ -0,0 +1,27 @@
+<template>
+	<v-btn
+		variant="tonal"
+		icon="mdi-chevron-up"
+		size="50px"
+		color="#04aa6d"
+		@click.stop="scrollToTop"
+		position="fixed"
+		class="button"
+	>
+	</v-btn>
+</template>
+<script setup>
+function scrollToTop() {
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	});
+}
+</script>
+<style scoped>
+.button {
+	bottom: 20px;
+	right: 20px;
+	z-index: 999;
+}
+</style>
