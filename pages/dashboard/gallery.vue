2a7b8367759fad4e053a0497dbffb2417c0d34de
frehsegustavo <frehsegustavo@gmail.com>
2023-12-31 01:05:01 -0300

    [add] Dashboard de galeria

---
+++ pages/dashboard/gallery.vue
@@ -0,0 +1,11 @@
+<template>
+	<v-container fluid>
+		<v-row> opa </v-row>
+	</v-container>
+</template>
+<script setup>
+definePageMeta({
+	layout: "dashboard",
+});
+</script>
+<style scoped></style>
