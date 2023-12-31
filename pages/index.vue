db2da99aa54171f5273893f0823c0fe843e18a72
frehsegustavo <frehsegustavo@gmail.com>
2023-12-31 01:04:42 -0300

    [add] Teste de requisição: get e post

--- pages/index.vue
+++ pages/index.vue
@@ -10,9 +10,35 @@
 		<Banner />
 		<!-- Bem vindo ao projeto base de 2024 :) -->
 		<Gallery />
+		{{ post }}
+		<br />
+		<v-btn @click="enviarDado">Mandar dado random</v-btn>
 	</v-container>
 </template>
 <script setup>
 import Banner from "@/components/sections/home/Banner.vue";
 import Gallery from "@/components/sections/home/Gallery.vue";
+
+// O massa que da para atrelar uma VARIAVEL de loading :)
+let postTeste = ref({
+	name: "teste",
+	imagem: "teste",
+});
+
+const { data: post, refresh } = await useAsyncData("get", () =>
+	$fetch(`http://localhost:8000/gallery`)
+);
+
+async function enviarDado() {
+	await $fetch("http://localhost:8000/gallery", {
+		method: "POST",
+		body: postTeste.value,
+	})
+		.then(() => {
+			console.log(`Enviado!`);
+		})
+		.catch((error) => {
+			console.log(`Erro: ${error}`);
+		});
+}
 </script>
