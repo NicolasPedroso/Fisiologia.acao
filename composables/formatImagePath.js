9c43de274f9a0ac7f6131eef73d7e9e73e1f838d
frehsegustavo <frehsegustavo@gmail.com>
2024-02-22 16:07:54 -0300

    [add] formatImagePath global function para formatar o caminho da imagem do back-end

---
+++ composables/formatImagePath.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+/**
+ * * Função para formatar a URL da imagem para o formato correto
+ *		- Troca o public/ por storage/ na URL da imagem
+ *  	- Utiliza o runtimeConfig para pegar a baseURL
+ *      - Exemplo: return "http://localhost:8000/storage/images/image.jpg"
+ *
+ * @param {String} url - URL da imagem a ser formatado
+ * @returns {String} - URL da imagem formatada (BaseURL + URL da imagem formatado)
+ */
+export const formatImagePath = (url) => {
+	const config = useRuntimeConfig()
+	const configURL = config.public.baseURL
+
+	// Troca o public/ por storage/ na URL da imagem
+	return `${configURL}/${url.replace("public/", "storage/")}`
+}
