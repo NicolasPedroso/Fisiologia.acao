8ffd7b690e7d8d6c58b6c381aafc759aa5e571af
frehsegustavo <frehsegustavo@gmail.com>
2024-02-26 01:11:25 -0300

    [ADD] Comentários e introdução ao projeto

--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -1,44 +1,55 @@
 <template>
-	<v-container fluid class="ma-0 pa-0 text-center">
-		<Banner :title="'Bem-vindo'" />
-		<WorkTypes />
-		<!-- Bem vindo ao projeto base de 2024 :) -->
-		<Gallery />
-		<WeOffer />
-		<v-divider />
-		<Testimony :testimonies="requisicao.testimonies" />
+	<v-container fluid class="ma-0 pa-0">
+		<!-- Quando for começar deletar tudo aqui dentro -->
+		<div
+			style="
+				background-color: #0b0b0b;
+				height: 100vh;
+				color: #fff;
+				gap: 20px;
+			"
+			class="text-center d-flex flex-column justify-center align-center"
+		>
+			<h1 style="color: #00dc80">Bem-vindo(a) ao Projeto Base de 2024</h1>
+			<p>
+				1. Olhem e pesquisem sobre a estrutura das Pastas NUXT no drive
+				de Projetos antes de iniciar
+			</p>
+			<p>
+				2. Utilizem o Vuetify, contém vários componentes e scripts
+				prontos
+			</p>
+			<p>
+				3. Para pegar dados do backend, vejam o uso do
+				<strong>useDataLoader</strong> que fizemos para vocês, ele está
+				documentado em composables/useDataLoader.js e veja bem como
+				$fetch e useFetch funciona no Nuxt3
+			</p>
+			<p>
+				4. Sigam o padrão de código estabelecido por Qualidade na
+				<a
+					href="https://wiki-ecomp.vercel.app/docs/Padrao/Carreiras/Front-End/"
+					target="_blank"
+					style="color: #00dc80"
+				>
+					wiki deles
+				</a>
+			</p>
+			<p>
+				5. Utilize e abuse da documentação do Nuxt3, Vue3, Vuetify e dos
+				materiais das capacitações
+			</p>
+			<p>
+				6.
+				<strong style="color: #00dc80">
+					E por fim, não hesitem em perguntar, somos todos ECOMP
+				</strong>
+			</p>
+		</div>
+		<!-- Quando for começar deletar tudo aqui dentro -->
 	</v-container>
 </template>
 <script setup>
-import Banner from "~/components/global/Banner.vue"
-import WorkTypes from "~/components/sections/home/WorkTypes.vue"
-import WeOffer from "~/components/sections/home/WeOffer.vue"
-import Testimony from "~/components/sections/home/Testimony.vue"
-import Gallery from "~/components/sections/home/Gallery.vue"
-
-const { data: requisicao } = await useAsyncData("api-testemunhos", async () => {
-	try {
-		const [testimonies] = await Promise.all([useDataLoader("/testimonies")])
-
-		return {
-			testimonies,
-		}
-	} catch (error) {
-		console.error(error)
-		return {
-			testimonies: [
-				{
-					id: 0,
-					name: "Nome",
-					from: "Fonte",
-					text: "Testemunho",
-					image: "/banco-testes/imagem-teste-03.jpg",
-				},
-			],
-		}
-	}
-})
-
 // Cabeçalhos da pagina
 useSeoMeta({
 	title: "Página inicial",
