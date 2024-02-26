8f2250281e0d994c31b49ce61488ea81bd795d55
frehsegustavo <frehsegustavo@gmail.com>
2024-02-26 00:37:23 -0300

    [add] UTIL para formatar erros de .catch em promises

---
+++ front-end/utils/formatError.js
@@ -0,0 +1,32 @@
+/**
+ * Função geral que pega erros do .catch (err) e retorna uma string com todos os erros do back-end laravel
+ *      * Os erros do back-end são retornados em error.response._data
+ *
+ * @param {Object} error - Objeto de erro retornado do .catch (err)
+ * @return {String} - String com todos os erros do back-end laravel concatenados por &
+ */
+export default (error) => {
+	// Verifica se o erro é um objeto com a propriedade response
+	// Caso não, apenas retorna o erro sem tratamento
+	if (!error.response) return error
+
+	// Extrair todas as strings de erro do back-end
+	const allErrors = []
+	let objetoErros = {}
+
+	// Verifica se existe a propriedade errors em error.response._data.errors
+	objetoErros = error.response._data.errors
+		? error.response._data.errors
+		: error.response._data
+
+	for (const i in objetoErros) {
+		// Itera sobre os elementos do array
+		objetoErros[i].forEach((item) => {
+			// Adiciona a string ao array de strings
+			allErrors.push(item)
+		})
+	}
+
+	// Retorna as strings concatenadas por &
+	return allErrors.join(" & ")
+}
