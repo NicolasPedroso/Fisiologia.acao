5854127430e7fd6aaee1f6dbe4e34fba5a09d7d9
frehsegustavo <frehsegustavo@gmail.com>
2024-02-26 01:10:57 -0300

    [fix] Classes desnecessárias removidas

--- front-end/layouts/auth.vue
+++ front-end/layouts/auth.vue
@@ -6,8 +6,7 @@
 				min-width="300px"
 				width="90vw"
 				max-width="600px"
-				color="rgba(255, 255, 255, 0.9)"
-				class="auth-card pa-4 pa-sm-10"
+				class="rounded-xl pa-4 pa-sm-10"
 			>
 				<NuxtPage />
 			</v-card>
@@ -18,12 +17,7 @@
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap");
 .auth-wrapper {
-	background: url("/imagens/banner.jpeg") no-repeat center center fixed;
-	filter: grayscale(50%);
-	background-size: cover;
-}
-
-.auth-card {
-	border-radius: 25px;
+	background: url("/imagens/auth/background.jpeg") no-repeat center center
+		fixed;
 }
 </style>
