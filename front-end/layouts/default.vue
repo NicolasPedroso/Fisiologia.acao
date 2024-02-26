5cb0e1e485c2d51b7bf9ff96790a67471ac565ba
frehsegustavo <frehsegustavo@gmail.com>
2024-02-26 01:11:07 -0300

    [del] Componentes inutilizados

--- front-end/layouts/default.vue
+++ front-end/layouts/default.vue
@@ -1,16 +1,6 @@
 <template>
 	<v-app style="overflow-x: hidden !important">
-		<Navbar />
 		<NuxtPage />
-		<!-- <Subscribe /> -->
-		<ButtonTop />
-		<Footer />
 	</v-app>
 </template>
-
-<script setup>
-import Navbar from "~/components/sections/layout/Navbar.vue"
-// import Subscribe from "~/components/sections/layout/Subscribe.vue";
-import ButtonTop from "~/components/sections/layout/ButtonTop.vue"
-import Footer from "~/components/sections/layout/Footer.vue"
-</script>
+<script setup></script>
