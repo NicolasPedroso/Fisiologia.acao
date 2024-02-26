62a24805c7636723e3f3975458ef158e02b25133
frehsegustavo <frehsegustavo@gmail.com>
2024-02-26 01:31:48 -0300

    [mod] Texto inicial com melhor gap

--- front-end/pages/dashboard/index.vue
+++ front-end/pages/dashboard/index.vue
@@ -4,16 +4,16 @@
 		style="height: 100%"
 		class="d-flex flex-column justify-center align-center text-center"
 	>
-		<h2>Bem-vindo a Dashboard</h2>
-		<h4 class="mt-2">
+		<h1>Bem-vindo a Dashboard</h1>
+		<h4 class="mt-6">
 			Esta é a dashboard do administrador. Aqui você pode gerenciar as
 			seções, páginas, dados dinâmicos da sua aplicação.
 		</h4>
-		<p class="mt-8">
+		<p class="mt-4">
 			As páginas que você pode acessar estão listadas no menu lateral a
 			esquerda.
 		</p>
-		<p class="mt-8">
+		<p class="mt-4">
 			Para acessar a dashboard, você precisa estar logado como
 			administrador. Caso queira sair da dashboard, clique no botão
 			"<strong>Sair</strong>" no canto superior direito da tela.
