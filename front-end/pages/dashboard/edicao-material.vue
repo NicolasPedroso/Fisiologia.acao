d290b1ef2d3e0eae94debe63c04ba767a2a5a86f
mTh22k <matheus132202@gmail.com>
2025-03-23 17:57:34 -0300

    requisição do novo back teste

--- front-end/pages/dashboard/edicao-material.vue
+++ front-end/pages/dashboard/edicao-material.vue
@@ -8,37 +8,20 @@
 				</template>
 				<!-- Logo e nome do Projeto -->
 				<v-list lines="3" class="my-2">
-					<v-list-item
-						class="drawer-logo"
-						title="Endo Games"
-						subtitle="O projeto de extensão FISIOLOGIA EM AÇÃO"
-					>
+					<v-list-item class="drawer-logo" title="Endo Games"
+						subtitle="O projeto de extensão FISIOLOGIA EM AÇÃO">
 						<template #prepend>
-							<v-img
-								width="40"
-								height="56"
-								contain
-								class="mr-4"
-								src="/images/logo.png"
-								alt="Logo Endo Games"
-							/>
+							<v-img width="40" height="56" contain class="mr-4" src="/images/logo.png"
+								alt="Logo Endo Games" />
 						</template>
 					</v-list-item>
 				</v-list>
 				<!-- Logo e nome do Projeto -->
 				<!-- Itens do drawer -->
 				<v-list nav>
-					<v-list-item
-						v-for="(item, i) in drawerItems"
-						:key="i"
-						:to="item.link"
-						:base-color="theme.drawerTextColor"
-						:prepend-icon="item.icon"
-						:title="item.title"
-						:subtitle="item.subtitle"
-						lines="3"
-						class="my-1 py-1 drawer-text"
-					/>
+					<v-list-item v-for="(item, i) in drawerItems" :key="i" :to="item.link"
+						:base-color="theme.drawerTextColor" :prepend-icon="item.icon" :title="item.title"
+						:subtitle="item.subtitle" lines="3" class="my-1 py-1 drawer-text" />
 				</v-list>
 				<!-- Itens do drawer -->
 			</v-navigation-drawer>
@@ -46,10 +29,7 @@
 		<!-- Drawer que contém os links para as páginas -->
 		<!-- Navbar que contém TITULO, DRAWER-OPENER e LOGOUT BTN -->
 		<v-app-bar class="primary-color">
-			<v-app-bar-nav-icon
-				class="text-color"
-				@click.stop="drawer = !drawer"
-			/>
+			<v-app-bar-nav-icon class="text-color" @click.stop="drawer = !drawer" />
 			<v-toolbar-title class="text-color">
 				Painel do Administrador
 			</v-toolbar-title>
@@ -65,18 +45,11 @@
 			</div>
 			<v-divider color="white" style="border-width: 1.5px" />
 			<div class="pa-4">
-				<v-btn color="success font-weight-black" @click="dialog = true"
-					>Adicionar nova pergunta</v-btn
-				>
+				<v-btn color="success font-weight-black" @click="dialog = true">Adicionar nova pergunta</v-btn>
 			</div>
 			<div>
 				<v-container fluid>
-					<v-data-table
-						disable-pagination
-						class="elevation-1"
-						:items="perguntas"
-						item-value="id"
-					>
+					<v-data-table disable-pagination class="elevation-1" :items="perguntas" item-value="id">
 						<template #headers>
 							<tr>
 								<th class="text-left">Ações</th>
@@ -95,23 +68,11 @@
 								<!-- Botões de ação -->
 								<td style="padding: 20px">
 									<div class="d-flex flex-row">
-										<v-btn
-											icon
-											class="mx-2"
-											@click="editarPergunta(item)"
-										>
-											<v-icon color="secondary"
-												>mdi-pencil</v-icon
-											>
+										<v-btn icon class="mx-2" @click="editarPergunta(item)">
+											<v-icon color="secondary">mdi-pencil</v-icon>
 										</v-btn>
-										<v-btn
-											icon
-											class="mx-2"
-											@click="verDetalhes(item)"
-										>
-											<v-icon color="white"
-												>mdi-eye-outline</v-icon
-											>
+										<v-btn icon class="mx-2" @click="verDetalhes(item)">
+											<v-icon color="white">mdi-eye-outline</v-icon>
 										</v-btn>
 									</div>
 								</td>
@@ -122,37 +83,24 @@
 								<td style="padding: 20px">{{ item.nome }}</td>
 								<td style="padding: 20px">
 									{{
-										item.alternativas.find(
-											(alt) => alt.correta,
-										)?.texto || "sem correta"
-									}}
+				item.alternativas.find(
+					(alt) => alt.correta,
+				)?.texto || "sem correta"
+			}}
 								</td>
 
 								<td style="padding: 20px">
 									{{ item.dificuldade }}
 								</td>
 								<td style="padding: 20px">
-									<a :href="item.link" target="_blank"
-										>Link</a
-									>
+									<a :href="item.link" target="_blank">Link</a>
 								</td>
 								<td style="padding: 20px">
-									<img
-										:src="item.imagem"
-										width="80"
-										height="80"
-										style="border-radius: 5px"
-									/>
+									<img :src="item.imagem" width="80" height="80" style="border-radius: 5px" />
 								</td>
 								<td style="padding: 20px">
-									<v-btn
-										icon
-										class="mx-2"
-										@click="deletaPergunta(item)"
-									>
-										<v-icon color="error"
-											>mdi-delete</v-icon
-										>
+									<v-btn icon class="mx-2" @click="deletaPergunta(item)">
+										<v-icon color="error">mdi-delete</v-icon>
 									</v-btn>
 								</td>
 							</tr>
@@ -165,90 +113,48 @@
 			<!-- DIALOG PARA ADICIONAR PERGUNTA -->
 			<v-dialog v-model="dialog" max-width="600px">
 				<v-card>
-					<v-card-title class="headline"
-						>Adicionar Pergunta</v-card-title
-					>
+					<v-card-title class="headline">Adicionar Pergunta</v-card-title>
 					<v-card-text>
 						<v-container>
 							<v-row>
 								<v-col cols="12">
-									<v-text-field
-										v-model="novaPergunta.id"
-										label="id (dps tirar)"
-										outlined
-										dense
-										type="number"
-									></v-text-field>
+									<v-text-field v-model="novaPergunta.id" label="id (dps tirar)" outlined dense
+										type="number"></v-text-field>
 								</v-col>
 								<v-col cols="12">
-									<v-text-field
-										v-model="novaPergunta.fase"
-										label="Fase"
-										outlined
-										dense
-										type="number"
-									></v-text-field>
+									<v-text-field v-model="novaPergunta.fase" label="Fase" outlined dense
+										type="number"></v-text-field>
 								</v-col>
 								<v-col cols="12">
-									<v-text-field
-										v-model="novaPergunta.nome"
-										label="Pergunta"
-										outlined
-										dense
-									></v-text-field>
+									<v-text-field v-model="novaPergunta.nome" label="Pergunta" outlined
+										dense></v-text-field>
 								</v-col>
 								<v-col cols="12">
-									<v-select
-										v-model="novaPergunta.dificuldade"
-										:items="['Fácil', 'Médio', 'Difícil']"
-										label="Dificuldade"
-										outlined
-										dense
-									></v-select>
+									<v-select v-model="novaPergunta.dificuldade" :items="['Fácil', 'Médio', 'Difícil']"
+										label="Dificuldade" outlined dense></v-select>
 								</v-col>
 								<v-col cols="12">
-									<v-text-field
-										v-model="novaPergunta.link"
-										label="Link do Vídeo"
-										outlined
-										dense
-									></v-text-field>
+									<v-text-field v-model="novaPergunta.link" label="Link do Vídeo" outlined
+										dense></v-text-field>
 								</v-col>
 								<v-col cols="12">
-									<v-text-field
-										v-model="novaPergunta.imagem"
-										label="imagem(link por enquanto)"
-										outlined
-										dense
-									></v-text-field>
+									<v-text-field v-model="novaPergunta.imagem" label="imagem(link por enquanto)"
+										outlined dense></v-text-field>
 								</v-col>
 
 								<!-- ALTERNATIVAS -->
 								<v-col cols="12">
-									<span class="font-weight-bold"
-										>Alternativas:</span
-									>
-									<v-row
-										v-for="(
+									<span class="font-weight-bold">Alternativas:</span>
+									<v-row v-for="(
 											alt, index
-										) in novaPergunta.alternativas"
-										:key="index"
-										align="center"
-									>
+										) in novaPergunta.alternativas" :key="index" align="center">
 										<v-col cols="8">
-											<v-text-field
-												v-model="alt.texto"
-												label="Alternativa"
-												outlined
-												dense
-											></v-text-field>
+											<v-text-field v-model="alt.texto" label="Alternativa" outlined
+												dense></v-text-field>
 										</v-col>
 										<v-col cols="4">
-											<v-checkbox
-												v-model="alt.correta"
-												label="Correta"
-												@change="marcarCorreta(index)"
-											></v-checkbox>
+											<v-checkbox v-model="alt.correta" label="Correta"
+												@change="marcarCorreta(index)"></v-checkbox>
 										</v-col>
 									</v-row>
 								</v-col>
@@ -256,12 +162,8 @@
 						</v-container>
 					</v-card-text>
 					<v-card-actions>
-						<v-btn color="red" text @click="dialog = false"
-							>Cancelar</v-btn
-						>
-						<v-btn color="green" text @click="adicionarPergunta()"
-							>Salvar</v-btn
-						>
+						<v-btn color="red" text @click="dialog = false">Cancelar</v-btn>
+						<v-btn color="green" text @click="adicionarPergunta()">Salvar</v-btn>
 					</v-card-actions>
 				</v-card>
 			</v-dialog>
@@ -269,74 +171,48 @@
 			<!-- Dialog para exibir detalhes da pergunta -->
 			<v-dialog v-model="dialogView" max-width="600px">
 				<v-card>
-					<v-card-title
-						class="headline text-h4 font-weight-bold d-flex justify-center"
-						>Detalhes da Pergunta</v-card-title
-					>
+					<v-card-title class="headline text-h4 font-weight-bold d-flex justify-center">Detalhes da
+						Pergunta</v-card-title>
 					<v-card-text>
 						<v-divider></v-divider>
 						<v-container>
 							<v-row>
 								<v-col cols="4">
-									<strong
-										class="pa-4 text-h6 font-weight-bold d-flex justify-center"
-										>ID</strong
-									>
-									<span
-										class="pa-4 text-subtitle-1 d-flex justify-center"
-									>
+									<strong class="pa-4 text-h6 font-weight-bold d-flex justify-center">ID</strong>
+									<span class="pa-4 text-subtitle-1 d-flex justify-center">
 										{{ perguntaSelecionada.id }}
 									</span>
 								</v-col>
 								<v-col cols="4">
-									<strong
-										class="pa-4 text-h6 font-weight-bold d-flex justify-center"
-										>Fase</strong
-									>
-									<span
-										class="pa-4 text-subtitle-1 d-flex justify-center"
-									>
+									<strong class="pa-4 text-h6 font-weight-bold d-flex justify-center">Fase</strong>
+									<span class="pa-4 text-subtitle-1 d-flex justify-center">
 										{{ perguntaSelecionada.fase }}
 									</span>
 								</v-col>
 								<v-col cols="4">
 									<strong
-										class="pa-4 text-h6 font-weight-bold d-flex justify-center"
-										>Dificuldade</strong
-									>
-									<span
-										class="pa-4 text-subtitle-1 d-flex justify-center"
-									>
+										class="pa-4 text-h6 font-weight-bold d-flex justify-center">Dificuldade</strong>
+									<span class="pa-4 text-subtitle-1 d-flex justify-center">
 										{{ perguntaSelecionada.dificuldade }}
 									</span>
 								</v-col>
 								<v-divider></v-divider>
 								<v-col cols="12">
 									<strong
-										class="pa-4 text-h6 font-weight-bold d-flex justify-center"
-										>Pergunta</strong
-									>
-									<span
-										class="pa-4 text-subtitle-1 d-flex justify-center"
-									>
+										class="pa-4 text-h6 font-weight-bold d-flex justify-center">Pergunta</strong>
+									<span class="pa-4 text-subtitle-1 d-flex justify-center">
 										{{ perguntaSelecionada.nome }}
 									</span>
 								</v-col>
 								<v-divider></v-divider>
 								<v-col cols="12">
 									<span
-										class="pa-4 text-h6 font-weight-bold d-flex justify-center"
-										>Alternativas</span
-									>
+										class="pa-4 text-h6 font-weight-bold d-flex justify-center">Alternativas</span>
 									<v-card class="pa-5">
 										<v-row class="d-flex justify-center">
-											<v-col
-												v-for="(
+											<v-col v-for="(
 													alt, index
-												) in perguntaSelecionada.alternativas"
-												:key="index"
-												cols="auto"
-											>
+												) in perguntaSelecionada.alternativas" :key="index" cols="auto">
 												<span v-if="index == 0">
 													a)
 												</span>
@@ -349,13 +225,10 @@
 												<span v-if="index == 3">
 													d)
 												</span>
-												<span
-													:class="
-														alt.correta
-															? 'text-green font-weight-bold'
-															: 'text-red'
-													"
-												>
+												<span :class="alt.correta
+					? 'text-green font-weight-bold'
+					: 'text-red'
+				">
 													{{ alt.texto }}
 												</span>
 											</v-col>
@@ -363,128 +236,71 @@
 									</v-card>
 								</v-col>
 								<v-col cols="12">
-									<strong
-										class="pa-4 text-h6 font-weight-bold d-flex justify-center"
-										>Link:</strong
-									>
-									<span
-										class="pa-4 text-subtitle-1 d-flex justify-center"
-									>
+									<strong class="pa-4 text-h6 font-weight-bold d-flex justify-center">Link:</strong>
+									<span class="pa-4 text-subtitle-1 d-flex justify-center">
 										{{ perguntaSelecionada.link }}
 									</span>
 								</v-col>
 								<v-divider></v-divider>
 								<v-col cols="12">
 									<strong
-										class="pa-4 text-h6 font-weight-bold d-flex justify-center"
-										>Imagem:</strong
-									><br />
-									<img
-										v-if="perguntaSelecionada.imagem"
-										:src="perguntaSelecionada.imagem"
-										width="100%"
-										style="border-radius: 5px"
-									/>
+										class="pa-4 text-h6 font-weight-bold d-flex justify-center">Imagem:</strong><br />
+									<img v-if="perguntaSelecionada.imagem" :src="perguntaSelecionada.imagem"
+										width="100%" style="border-radius: 5px" />
 								</v-col>
 							</v-row>
 						</v-container>
 					</v-card-text>
 					<v-card-actions>
 						<v-spacer />
-						<v-btn color="red" text @click="dialogView = false"
-							>Fechar</v-btn
-						>
+						<v-btn color="red" text @click="dialogView = false">Fechar</v-btn>
 					</v-card-actions>
 				</v-card>
 			</v-dialog>
 
 			<v-dialog v-model="dialogEdit" max-width="600px">
 				<v-card>
-					<v-card-title class="headline"
-						>Editar Pergunta</v-card-title
-					>
+					<v-card-title class="headline">Editar Pergunta</v-card-title>
 					<v-card-text>
 						<v-container>
 							<v-row>
 								<v-col cols="12">
-									<v-text-field
-										v-model="perguntaEdit.id"
-										label="ID"
-										outlined
-										dense
-										type="number"
-										disabled
-									></v-text-field>
+									<v-text-field v-model="perguntaEdit.id" label="ID" outlined dense type="number"
+										disabled></v-text-field>
 								</v-col>
 								<v-col cols="12">
-									<v-text-field
-										v-model="perguntaEdit.fase"
-										label="Fase"
-										outlined
-										dense
-										type="number"
-									></v-text-field>
+									<v-text-field v-model="perguntaEdit.fase" label="Fase" outlined dense
+										type="number"></v-text-field>
 								</v-col>
 								<v-col cols="12">
-									<v-text-field
-										v-model="perguntaEdit.nome"
-										label="Pergunta"
-										outlined
-										dense
-									></v-text-field>
+									<v-text-field v-model="perguntaEdit.nome" label="Pergunta" outlined
+										dense></v-text-field>
 								</v-col>
 								<v-col cols="12">
-									<v-select
-										v-model="perguntaEdit.dificuldade"
-										:items="['Fácil', 'Médio', 'Difícil']"
-										label="Dificuldade"
-										outlined
-										dense
-									></v-select>
+									<v-select v-model="perguntaEdit.dificuldade" :items="['Fácil', 'Médio', 'Difícil']"
+										label="Dificuldade" outlined dense></v-select>
 								</v-col>
 								<v-col cols="12">
-									<v-text-field
-										v-model="perguntaEdit.link"
-										label="Link do Vídeo"
-										outlined
-										dense
-									></v-text-field>
+									<v-text-field v-model="perguntaEdit.link" label="Link do Vídeo" outlined
+										dense></v-text-field>
 								</v-col>
 								<v-col cols="12">
-									<v-text-field
-										v-model="perguntaEdit.imagem"
-										label="Imagem (Link)"
-										outlined
-										dense
-									></v-text-field>
+									<v-text-field v-model="perguntaEdit.imagem" label="Imagem (Link)" outlined
+										dense></v-text-field>
 								</v-col>
 								<v-col cols="12">
-									<span class="font-weight-bold"
-										>Alternativas:</span
-									>
-									<v-row
-										v-for="(
+									<span class="font-weight-bold">Alternativas:</span>
+									<v-row v-for="(
 											alt, index
-										) in perguntaEdit.alternativas"
-										:key="index"
-										align="center"
-									>
+										) in perguntaEdit.alternativas" :key="index" align="center">
 										<v-col cols="8">
-											<v-text-field
-												v-model="alt.texto"
-												label="Alternativa"
-												outlined
-												dense
-											></v-text-field>
+											<v-text-field v-model="alt.texto" label="Alternativa" outlined
+												dense></v-text-field>
 										</v-col>
 										<v-col cols="4">
-											<v-checkbox
-												v-model="alt.correta"
-												label="Correta"
-												@change="
-													marcarCorretaEdit(index)
-												"
-											></v-checkbox>
+											<v-checkbox v-model="alt.correta" label="Correta" @change="
+				marcarCorretaEdit(index)
+				"></v-checkbox>
 										</v-col>
 									</v-row>
 								</v-col>
@@ -492,12 +308,8 @@
 						</v-container>
 					</v-card-text>
 					<v-card-actions>
-						<v-btn color="red" text @click="dialogEdit = false"
-							>Cancelar</v-btn
-						>
-						<v-btn color="green" text @click="atualizarPergunta()"
-							>Salvar</v-btn
-						>
+						<v-btn color="red" text @click="dialogEdit = false">Cancelar</v-btn>
+						<v-btn color="green" text @click="atualizarPergunta()">Salvar</v-btn>
 					</v-card-actions>
 				</v-card>
 			</v-dialog>
@@ -521,6 +333,8 @@ import axios from "axios"
 
 // Variável reativa para armazenar as perguntas
 const perguntas = ref([])
+const fases = ref([])
+const respostas = ref([])
 const dialog = ref(false) // Estado do diálogo
 const dialogView = ref(false) // Estado do diálogo
 const perguntaSelecionada = ref({})
@@ -556,13 +370,31 @@ const perguntaEdit = ref({
 })
 
 // Função para buscar os dados da API
+const fetchFases = async () => {
+	try {
+		const response1 = await axios.get("http://localhost:8000/api/fases")
+		fases.value = response1.data // Armazena os dados na variável reativa
+		console.log("fase", fases.value[0])
+	} catch (error) {
+		console.error("Erro ao buscar ddasdases:", error)
+	}
+}
 const fetchPerguntas = async () => {
 	try {
-		const response = await axios.get("http://localhost:8000/perguntas")
-		perguntas.value = response.data // Armazena os dados na variável reativa
-		console.log("aaaa", perguntas.value[0])
+		const response2 = await axios.get("http://localhost:8000/api/perguntas")
+		perguntas.value = response2.data.data // Armazena os dados na variável reativa
+		console.log("pergunta", perguntas.value)
 	} catch (error) {
-		console.error("Erro ao buscar perguntas:", error)
+		console.error("Erro ao buscar daaaaaaaases:", error)
+	}
+}
+const fetchRespostas = async () => {
+	try {
+		const response3 = await axios.get("http://localhost:8000/api/respostas")
+		respostas.value = response3.data // Armazena os dados na variável reativa
+		console.log("respostas", respostas.value)
+	} catch (error) {
+		console.error("Erro ao buscar dbnases:", error)
 	}
 }
 
@@ -748,6 +580,8 @@ const verDetalhes = (item) => {
 // Chama a função quando o componente é montado
 onMounted(() => {
 	fetchPerguntas()
+	fetchFases()
+	fetchRespostas()
 })
 
 // Variáveis de ambiente
